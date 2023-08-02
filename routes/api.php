29fdb129b7fcd78f9fb30272805bb8f6ee7a5f6e
XEZO3 <75046635+XEZO3@users.noreply.github.com>
2023-08-02 23:41:19 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,54 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/user/register',[authController::class,'register']);
-Route::post('/user/login',[authController::class,'login'])->name("login");
 Auth::routes(['verify' => true]);
-Route::get('/user/profile',function(Request $req){
-    return"you are verified";
-})->middleware(["auth:sanctum",'verified']);
-Route::get('/user/email/verify', function () {
-     return ["message"=>"verify the email@@"];;
-})->middleware('auth:sanctum')->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+Route::post('/user/register',[authController::class,'register']);
+Route::post('/user/login',[authController::class,'login']);
+// Route::get('/user/email/verify', function () {
+//      return ["message"=>"verify the email@@"];;
+// })->middleware('auth:sanctum')->name('verification.notice');
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
+//     return [
+//         'returnCode'=>200,
+//         "message"=>"done",
+//         "results"=>[]
+//         ];
+// })->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+
+// Route::post('/user/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
+//     return ["message"=>"new email was sent"];
+// })->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
+
+Route::group(['middleware'=>['auth:api']],function(){
+    Route::get('/user/logout',[authController::class,'logout']);
+    
+        Route::get('/user/profile',function(Request $request ){
+            return "testing";
+        })->middleware(['verified']);
+
+    
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
- 
-    return ["message"=>"done"];
-})->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+    return [
+        'returnCode'=>200,
+        "message"=>"done",
+        "results"=>[]
+        ];
+})->middleware(['signed'])->name('verification.verify');
 
 Route::post('/user/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return ["message"=>"new email was sent"];
-})->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
+})->middleware(['throttle:6,1'])->name('verification.send');
 
-Route::group(['middleware'=>['auth:sanctum']],function(){
-    Route::get('/user/logout',[authController::class,'logout']);
+// Route::get('/user/email/verify', function (Request $request) {
+//      if(auth()->user()->hasVerifiedEmail()){
+//          return ["message"=>"true"];
+//      }else{
+//         return ["message"=>"false"];
+//      }
+// });
 });
 
